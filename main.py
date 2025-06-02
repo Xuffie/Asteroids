6c720af8d6018a530e02298c6f8561383f0a9d04
Xuffie <xuffie@cricketpit.com>
2025-06-02 19:32:35 -0400

    Added shooting mechanic

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from player import *
 from asteroid import *
 from asteroidfield import *
 from circleshape import *
+from shot import *
 
 def main():
     pygame.init()
@@ -18,8 +19,11 @@ def main():
     updateable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
 
+    shots = pygame.sprite.Group()
+
     asteroids_group = pygame.sprite.Group()
 
+    Shot.containers = (shots, updateable, drawable)
     Player.containers = (updateable, drawable)
     Asteroid.containers = (asteroids_group, updateable, drawable)
     AsteroidField.containers = (updateable)
@@ -28,13 +32,15 @@ def main():
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
+    # bullet = Shot(player.position[0], player.position[1])
+
     while 1 > 0:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
         updateable.update(dt)
-
+        player.shot_cd = player.shot_cd - dt
         for asteroid_obj in asteroids_group:
             if player.collision_checker(asteroid_obj) == True:
                 print("Game over!")
--- player.py
+++ player.py
@@ -1,11 +1,13 @@
 import pygame
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_COOLDOWN
 from circleshape import *
+from shot import *
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shot_cd = 0
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -35,7 +37,16 @@ class Player(CircleShape):
 
         if keys[pygame.K_s]:
             self.move(-dt)
+        
+        if keys[pygame.K_SPACE]:
+            self.shoot()
 
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
+
+    def shoot(self):
+        if self.shot_cd <= 0:
+            new_shot = Shot(self.position[0], self.position[1])
+            new_shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+            self.shot_cd = PLAYER_SHOOT_COOLDOWN
