a98ccf40acb010da72a7217ae17458c64a1ddf65
Xuffie <xuffie@cricketpit.com>
2025-06-02 15:16:51 -0400

    created player, drawn, and able to move

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
 
     game_time = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -14,8 +16,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
             
+           
         pygame.Surface.fill(screen,(0,0,0))
+        player.draw(screen) 
+        player.update(dt)
         pygame.display.flip()
+
         
         dt = game_time.tick(60) / 1000
 
