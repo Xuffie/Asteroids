9e849ab5f9f6c05a6e5211e356007b65e215195e
Xuffie <xuffie@cricketpit.com>
2025-06-02 14:35:30 -0400

    create FPS indicator

--- main.py
+++ main.py
@@ -3,18 +3,23 @@ from constants import *
 
 def main():
     pygame.init()
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
+    game_time = pygame.time.Clock()
+
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while 1 > 0:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         pygame.Surface.fill(screen,(0,0,0))
-        pygame.display.flip
+        pygame.display.flip()
+        
+        dt = game_time.tick(60) / 1000
+
+
 
 if __name__ == "__main__":
     main()
